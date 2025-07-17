94346139177867f2758ac1cf7c5b9d947a4af31c
tudtbis <tudinh.bis@outlook.com>
2025-07-17 19:29:13 +0700

    Add setBait command to configure default bait type in agent

--- src/commands/sbait.ts
+++ src/commands/sbait.ts
@@ -2,11 +2,11 @@ import { BaseAgent } from "../structures/BaseAgent.js";
 import { Commands } from "../typings/typings.js";
 
 const setBaitCommand: Commands = {
-	name: "setbait",
+	name: "sbait",
 	description: "Thiết lập loại mồi mặc định để mua khi hết mồi",
 	execute: async (agent: BaseAgent, message, ...args: string[]) => {
 		if (!args[0]) {
-			return message.reply(`❌ Vui lòng chỉ định loại mồi! Sử dụng: setbait <m1|m2|m3|m4|m5>\nLoại mồi hiện tại: **${agent.defaultBait}**`);
+			return message.reply(`❌ Vui lòng chỉ định loại mồi! Sử dụng: sbait <m1|m2|m3|m4|m5>\nLoại mồi hiện tại: **${agent.defaultBait}**`);
 		}
 
 		const baitType = args[0].toLowerCase();
