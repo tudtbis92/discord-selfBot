1fa1f877477a96ac837a4a96449f3efede231792
tudt92 <tudinh.bis@gmail.com>
2026-05-19 15:08:04 +0700

    chore: add pooh.jinping app configuration and reformat ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,44 +1,54 @@
 module.exports = {
-  apps: [
-    {
-      name: "trumpet.donald",
-      script: "./dest/index.js",
-      args: "-i config1.json",
-      out_file: "./logs/trumpet-out.log",
-      error_file: "./logs/trumpet-error.log",
-      merge_logs: true,
-      autorestart: true,
-      env: { NODE_ENV: "production" }
-    },
-    {
-      name: "pu.tintin",
-      script: "./dest/index.js",
-      args: "-i config2.json",
-      out_file: "./logs/putin-out.log",
-      error_file: "./logs/putin-error.log",
-      merge_logs: true,
-      autorestart: true,
-      env: { NODE_ENV: "production" }
-    },
-    {
-      name: "jongun.kim",
-      script: "./dest/index.js",
-      args: "-i config3.json",
-      out_file: "./logs/jongun-out.log",
-      error_file: "./logs/jongun-error.log",
-      merge_logs: true,
-      autorestart: true,
-      env: { NODE_ENV: "production" }
-    },
-    {
-      name: "tolam.csvn",
-      script: "./dest/index.js",
-      args: "-i config4.json",
-      out_file: "./logs/tolam-out.log",
-      error_file: "./logs/tolam-error.log",
-      merge_logs: true,
-      autorestart: true,
-      env: { NODE_ENV: "production" }
-    }
-  ]
-}
+	apps: [
+		{
+			name: 'trumpet.donald',
+			script: './dest/index.js',
+			args: '-i config1.json',
+			out_file: './logs/trumpet-out.log',
+			error_file: './logs/trumpet-error.log',
+			merge_logs: true,
+			autorestart: true,
+			env: { NODE_ENV: 'production' },
+		},
+		{
+			name: 'pu.tintin',
+			script: './dest/index.js',
+			args: '-i config2.json',
+			out_file: './logs/putin-out.log',
+			error_file: './logs/putin-error.log',
+			merge_logs: true,
+			autorestart: true,
+			env: { NODE_ENV: 'production' },
+		},
+		{
+			name: 'pooh.jinping',
+			script: './dest/index.js',
+			args: '-i config5.json',
+			out_file: './logs/pooh-out.log',
+			error_file: './logs/pooh-error.log',
+			merge_logs: true,
+			autorestart: true,
+			env: { NODE_ENV: 'production' },
+		},
+		{
+			name: 'jongun.kim',
+			script: './dest/index.js',
+			args: '-i config3.json',
+			out_file: './logs/jongun-out.log',
+			error_file: './logs/jongun-error.log',
+			merge_logs: true,
+			autorestart: true,
+			env: { NODE_ENV: 'production' },
+		},
+		{
+			name: 'tolam.csvn',
+			script: './dest/index.js',
+			args: '-i config4.json',
+			out_file: './logs/tolam-out.log',
+			error_file: './logs/tolam-error.log',
+			merge_logs: true,
+			autorestart: true,
+			env: { NODE_ENV: 'production' },
+		},
+	],
+};
